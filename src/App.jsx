f5b063bbd41b078a6b4479e5ca55892fc6c6303e
Ellie Banks <ellenebanks5@gmail.com>
2021-07-08 09:36:26 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -17,29 +17,22 @@ class App extends React.Component {
             
             myChirps: [
                 ...this.state.myChirps,
-                { id: uuidv4(), chirp: 'MyNameIs21: Hi friends! Wuts up?' },
-                { id: uuidv4(), chirp: 'GoJaguars2010: n2m wbu?' },
-                { id: uuidv4(), chirp: 'HELLOWORLD365: n2m. Been super busy trying to code today...it is hard learning this stuff! I just love 2 chirp!' },
-                { id: uuidv4(), chirp: 'GoJaguars2010: me too. I g2g though, ttyl!' },
+                { id: uuidv4(), username: 'MyNameIs21', chirp: 'Hi friends! Wuts up?' },
+                { id: uuidv4(), username: 'GoJaguars2010', chirp: 'n2m wbu?' },
+                { id: uuidv4(), username: 'HELLOWORLD365', chirp: 'n2m. Been super busy trying to code today...it is hard learning this stuff! I just love 2 chirp!' },
+                { id: uuidv4(), username: 'GoJaguars2010', chirp: 'me too. I g2g though, ttyl!' },
 
             ]
         });
     }
 
-   /*} handleUsername(event) {
-        event.preventDefault();
-        this.setState({
-            username: '',
-            myChirps: [ ...this.state.myChirps, { id: uuidv4(), username: this.state.username}]
-        })
-    }*/
 
     handleSubmit(e) {
         e.preventDefault();
         this.setState({
             username: '',
             chirp: '',
-            myChirps: [...this.state.myChirps,  { id: uuidv4(), chirp: this.state.chirp }]
+            myChirps: [...this.state.myChirps,  { id: uuidv4(), username: this.state.username, chirp: this.state.chirp }]
         });
     }
 
@@ -48,10 +41,10 @@ class App extends React.Component {
 
             <main className="container-fluid bg-light">
                 <section className="row justify-content-center mt-5">
-                    <div className="col-md-6">
+                    <div className="col-md-6 mt-3 font-monospace">
                         <label className="display-6">Chirper</label>
 
-                        <div className="input-group mb-3">
+                        <div className="input-group shadow bg-body rounded mt-3 mb-3">
                             <span className="input-group-text" id="basic-addon1">@</span>
                             <input
                                 className="form-control" placeholder="Username" aria-label="Username"
@@ -62,40 +55,40 @@ class App extends React.Component {
                         </div>
                         
 
-                        <form className="form-group">
+                        <form className="form-group shadow bg-body rounded">
                             <textarea
                                 placeholder="My chirp..."
                                 value={this.state.chirp}
                                 onChange={e => this.setState({ chirp: e.target.value })}
                                 className="form-control"
                             />
+                        </form>
 
-                            <button onClick={e => this.handleSubmit(e)} className="btn-lg btn-primary mt-3">
+                            <button onClick={e => this.handleSubmit(e)} className="btn-lg btn-primary shadow mt-3">
                                 Chirp!
                             </button>
 
-                        </form>
                     </div>
                 </section>
                 <br />
                 <section className="row justify-content-center mt-3">
-                    <div className="col-md-6">
+                    <div className="col-md-6 font-monospace">
                         <label className="display-6 mb-3">Timeline</label>
                         
                     
-                        <ul className="list-group">
+                        <ul className="list-group shadow bg-body rounded">
                             {this.state.myChirps.map(myChirp => (
                                 <li className="list-group-item" key={`myChirps-chirp-${myChirp.id}`}>
-                                    {myChirp.chirp}</li>
+                                    <span className="fw-bold fs-5 text-primary">@{myChirp.username}: </span>
+                                    {myChirp.chirp}
+                                </li>
                                 
                             ))}
                         </ul>
                     </div>
-
                 </section>
-                            <br />
-                            <br />
-                            <br />
+                    <br />
+                    <br />         
             </main>
 
         );
